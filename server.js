01a3e4c0a8576985f2eb4ac87e1b2d0845dc1fdd
Duncan Lo <>
2018-08-09 17:24:19 -0600

    Made changes to package.json files. Installed file-system module and using it to test if information from json file in public folder can be found and sent to AS3 client.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const SocketServer = require('ws').Server;
 const WebSocket = require('ws');
 const path = require('path');
+const fs = require('file-system');
 
 const PORT = process.env.PORT || 3000;
 
@@ -12,12 +13,19 @@ const server = express()
 
 const wss = new SocketServer({ server });
 
+var testFile = fs.readFileSync('public/test.json');
+var contents = JSON.parse(testFile);
+
 wss.on('connection', (ws) => {
-  console.log('Client connected'); 
+
+  console.log('Client connected');
+
   ws.on('message', function incoming(data) {
 	  wss.clients.forEach(function each(client) {
       if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
+
+        client.send(contents.testelement);
+
       }
 	  });
   });
